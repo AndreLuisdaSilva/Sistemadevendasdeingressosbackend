69e21c38568b701aacd767ed61fa79b434635ebf
AndreLuisdaSilva <73311945+AndreLuisdaSilva@users.noreply.github.com>
2025-02-06 11:52:17 -0300

    Delete consu.mjs

--- consu.mjs
+++
@@ -1,9 +0,0 @@
-import {area, circus as Circue} from "./define.mjs";
-
-console.log(`A areá do circulo: ${area(6)}`)
-
-console.log(`A areá do circulo 1: ${Circue(6)}`)
-
-import * as Circos from "./define.mjs";
-
-console.log(`A areá do circulo 2: ${Circos.area(6)}`)
